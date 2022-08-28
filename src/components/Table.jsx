b050476705739a2be86802a7e02b4a30e52b4475
Matheus <matheuseduardo.jp@gmail.com>
2022-08-28 20:48:39 -0300

    Requisito 07 concluido

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <table>
         <thead>
@@ -15,9 +18,45 @@ class Table extends Component {
           <th>Moeda de conversão</th>
           <th>Editar/Excluir</th>
         </thead>
+        { expenses.map((despesa, index) => (
+          <tbody key={ index }>
+            <tr>
+              <td>{ despesa.description }</td>
+              <td>{ despesa.tag }</td>
+              <td>{ despesa.method }</td>
+              <td>
+                { despesa.exchangeRates[despesa.currency].code }
+                { Number(despesa.value).toFixed(2) }
+              </td>
+              <td>{ despesa.exchangeRates[despesa.currency].name }</td>
+              <td>
+                { despesa.exchangeRates[despesa.currency].code }
+                { Number(despesa.exchangeRates[despesa.currency].ask).toFixed(2) }
+              </td>
+              <td>
+                { despesa.exchangeRates[despesa.currency].codein }
+                { (Number(despesa.exchangeRates[despesa.currency].ask)
+                  * Number(despesa.value)).toFixed(2) }
+              </td>
+              <td>Real</td>
+              <td>
+                <button type="button">Editar</button>
+                <button type="button">Excluir</button>
+              </td>
+            </tr>
+          </tbody>
+        )) }
       </table>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.shape).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
